74d25567c9ee52c97b844f764c9bd7088dfd3409
Cameron May <camerondmay@gmail.com>
2020-02-29 15:59:29 +1300

    Update colourscape.js

--- colourscape.js
+++ colourscape.js
@@ -1,20 +1,28 @@
-  let isMousedown = 0;
+
   let vertices = [],
     pressureStore = [],
     brush = [];
+
   // brush mechanics
   let dx, dy;
+
   //time delay
   let milliCounter;
   let milliTrack = 0;
+
   //colour variables
   let colHue, colSat, colBri;
   let colOpacity = 0.4;
   let colourBool = 0;
 
+  // domain for randomisation
   let r1 = -5;
   let r2 = 10;
 
+  // global mouse track
+  let isMousedown = 0;
+
+  // colour variables
   let cloudHSB = [
     [180, 47, 25],
     [178, 23, 55],
@@ -22,6 +30,7 @@
     [164, 12, 95],
     [176, 45, 19]
   ];
+
   let sunsetHSB = [
     [11, 53, 96],
     [13, 83, 91],
@@ -29,6 +38,7 @@
     [334, 81, 91],
     [300, 67, 99]
   ];
+
   let brushRandomiser = 0;
   let autoX = 0,
     autoY = 0,
@@ -45,25 +55,35 @@
   }
 
   function setup() {
+
+    // make canvas and create all Layers
     createCanvas(windowWidth, windowHeight);
     textLayer = createGraphics(width, height);
     paintLayer = createGraphics(width, height);
     autoLayer = createGraphics(width, height);
-    textLayer = createGraphics(windowWidth, windowHeight);
+    textLayer = createGraphics(width, height);
     calcDimensions();
-    pixelDensity(1); // Ignores retina displays
-    imageMode(CENTER); // centers loaded brushes
-    blendMode(BLEND); // consider overlay and multiply
+
+    // aesthetics, and uniform colour paramaters
+    pixelDensity(1);
+    imageMode(CENTER);
+    blendMode(BLEND);
     colorMode(HSB, 360, 100, 100, 1);
     paintLayer.colorMode(HSB, 360, 100, 100);
     autoLayer.colorMode(HSB, 360, 100, 100);
     colHue = random(360);
     colSat = random(255);
+    autobrushQuality();
+
+    // backing colours
     backdrop();
     autoLayer.background(352, 68, 89, 100);
-    autoSetProperties();
+
+    // begin slideshow
     slide = 0;
     slideShow();
+
+    // all event listeners
     canvas.addEventListener('touchmove', moved);
     canvas.addEventListener('mousemove', moved);
     canvas.addEventListener('touchstart', touchdown);
@@ -75,10 +95,10 @@
 
   function backdrop() {
     noTint();
-    image(bg, windowWidth / 2, windowHeight / 2, windowWidth, windowHeight); // display backgrond
+    image(bg, width / 2, height / 2, width, height);
   }
 
-  function setProperties(_x, _y) {
+  function brushQuality(_x, _y) {
     brushRandomiser = int(random(1, 20));
     let swatchTemp = int(random(0, 5));
     if (colourBool) {
@@ -116,9 +136,9 @@
     }
   }
 
-  function autoSetProperties() {
-    setProperties();
-    setTimeout(autoSetProperties, 2000);
+  function autobrushQuality() {
+    brushQuality();
+    setTimeout(autobrushQuality, 2000);
   }
 
   function touchdown(ev) {
@@ -141,7 +161,7 @@
     if (!isMousedown) return;
 
     if (introState < 3){
-      makeDrawing(mouseX, mouseY);
+      makeDrawing(mouseX, mouseY, pmouseX, pmouseY);
     }
 
    else {
@@ -171,22 +191,15 @@
   function autoDraw() {
     pautoX = autoX;
     pautoY = autoY;
-
-
-
-
-
     autoX = autoX + (random(r1*1.2, r2*1.2));
     autoY = autoY + (random(r1, r2));
     makeDrawing(autoX % width, autoY % height, pautoX % width, pautoY % height);
-
-
   }
 
   function makeDrawing(_x, _y, pX, pY) {
-    const rotateDrift = 0.2
+        const rotateDrift = 0.2
     var angle1 = atan2(dy, dx) + (random(-rotateDrift, rotateDrift));
-    var segLength = windowWidth / 40;
+    var segLength = width / 40;
     var scalar = 30;
     let milliComp = 5;
     let tempX = 100;
@@ -200,7 +213,7 @@
       dy = _y - tempY;
       tempX = _x - (cos(angle1) * segLength / 2);
       tempY = _y - (sin(angle1) * segLength / 2);
-      scalar = constrain(300 * (random(3, abs(_x - pX)) / windowWidth), 0.2, 1.2);
+      scalar = constrain(300 * (random(3, abs(_x - pX)) / width), 0.1, 0.7);
       segment(tempX, tempY, angle1, brush[brushRandomiser], scalar)
       milliTrack = milliCounter;
     }
@@ -224,25 +237,23 @@
   function reset() {
     paintLayer.clear();
     colourBool = !colourBool;
-    setProperties();
+    brushQuality();
     console.log(colourBool);
     autoCopy();
   }
 
   function windowResized() {
-
     resizeCanvas(width, height);
     backdrop();
     textLayer.resizeCanvas(width, height);
     paintLayer.resizeCanvas(width, height);
-    autoLayer.resizeCanvas(width, height);
+    //autoLayer.resizeCanvas(width, height);
+    autoCopy();
     calcDimensions();
-
     if (introState === 3) {
       removeElements();
       writeTextUI();
     }
-
   }
 
   getPressure = function(ev) {
